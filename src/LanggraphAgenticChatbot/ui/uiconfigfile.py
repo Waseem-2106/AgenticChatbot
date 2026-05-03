67a765efbaf9b324971506be5b9673f26abdabb9
waseem-2106 <wasimshadow12@gmail.com>
2026-05-03 22:19:03 +0530

    fix error

--- src/LanggraphAgenticChatbot/ui/uiconfigfile.py
+++ src/LanggraphAgenticChatbot/ui/uiconfigfile.py
@@ -9,7 +9,7 @@ class Config:
     def get_llm_options(self):
         return (self.config["DEFAULT"].get("LLM_OPTIONS")or " ").split(", ")
     def get_usecase_options(self):
-        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+        return (self.config["DEFAULT"].get("USECASE_OPTIONS")or " ").split(", ")
     def get_groq_model_options(self):
         return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
     def get_page_title(self):
